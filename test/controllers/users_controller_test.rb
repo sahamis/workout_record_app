48ed79ce7fdf88c67c2ba5b049afe0fe0c5963a8
Masashi Ito <cbohwos.i@gmail.com>
2024-03-21 17:32:24 +0900

    Add account activations

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,60 +1,67 @@
 require "test_helper"
 
-class UsersControllerTest < ActionDispatch::IntegrationTest
+class UsersIndex < ActionDispatch::IntegrationTest
 
-  def setup 
-    @user = users(:michael)
-    @other_user = users(:archer)
+  def setup
+    @admin     = users(:michael)
+    @non_admin = users(:archer)
   end
+end
 
-  test "should get new" do
-    get signup_path
-    assert_response :success
-  end
+class UsersIndexAdmin < UsersIndex
 
-  test "should redirect edit when not logged in as wrong user" do
-    log_in_as(@other_user)
-    get edit_user_path(@user)
-    assert flash.empty?
-    assert_redirected_to root_url
-  end
-
-  test "should redirect update when not logged in as wrong user" do
-    log_in_as(@other_user)
-    patch user_path(@user), params: { user: { name:@user.name,
-                                             email:@user.email } }
-    assert flash.empty?
-    assert_redirected_to root_url
-  end
-
-  test "should redirect index when not logged in" do
+  def setup
+    super
+    log_in_as(@admin)
     get users_path
-    assert_redirected_to login_url
+  end
+end
+
+class UsersIndexAdminTest < UsersIndexAdmin
+
+  test "should render the index page" do
+    assert_template 'users/index'
   end
 
-  test "should not allow the admin attribute to be edited via the web" do
-    log_in_as(@other_user)
-    assert_not @other_user.admin?
-    patch user_path(@other_user), params: { user: { password:"password",
-                                                    password_confirmation: "password",
-                                                    admin: true } }
-    assert_not @other_user.reload.admin?
+  test "should paginate users" do
+    assert_select 'ul.pagination', count:2
   end
 
-  test "should redirect destroy when not logged in" do
-    assert_no_difference 'User.count' do
-      delete user_path(@user)
+  test "should have delete links" do
+    first_page_of_users = User.where(activated: true).paginate(page: 1)
+    first_page_of_users.each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      end
+    end
+  end
+
+  test "should be able to delete non-admin user" do
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
     end
     assert_response :see_other
-    assert_redirected_to login_url
+    assert_redirected_to users_url
   end
 
-  test "should redirect destroy when logged in as a non-admin" do
-    log_in_as(@other_user)
-    assert_no_difference 'User.count' do
-      delete user_path(@user)
+  test "should display only activated users" do
+    # ページにいる最初のユーザーを無効化する。
+    User.paginate(page: 1).first.toggle!(:activated)
+    # /usersを再度取得して、無効化済みのユーザーが表示されていないことを確かめる
+    get users_path      
+    # 表示されているすべてのユーザーが有効化済みであることを確かめる
+    assigns(:users).each do |user|
+      assert user.activated?
     end
-    assert_response :see_other
-    assert_redirected_to root_url
+  end
+end
+
+class UsersNonAdminIndexTest < UsersIndex
+
+  test "should not have delete links as non-admin" do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
   end
 end
