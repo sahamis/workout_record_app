f30a5fc43807e0c90fa8e47c5a4aacfcd5c18d44
Masashi Ito <cbohwos.i@gmail.com>
2024-04-03 21:07:57 +0900

    Add user following

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,67 +1,70 @@
 require "test_helper"
 
-class UsersIndex < ActionDispatch::IntegrationTest
+class UsersControllerTest < ActionDispatch::IntegrationTest
 
-  def setup
-    @admin     = users(:michael)
-    @non_admin = users(:archer)
+  def setup 
+    @user = users(:michael)
+    @other_user = users(:archer)
   end
-end
 
-class UsersIndexAdmin < UsersIndex
+  test "should get new" do
+    get signup_path
+    assert_response :success
+  end
 
-  def setup
-    super
-    log_in_as(@admin)
+  test "should redirect edit when not logged in as wrong user" do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when not logged in as wrong user" do
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: { name:@user.name,
+                                             email:@user.email } }
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect index when not logged in" do
     get users_path
-  end
-end
-
-class UsersIndexAdminTest < UsersIndexAdmin
-
-  test "should render the index page" do
-    assert_template 'users/index'
+    assert_redirected_to login_url
   end
 
-  test "should paginate users" do
-    assert_select 'ul.pagination', count:2
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: { user: { password:"password",
+                                                    password_confirmation: "password",
+                                                    admin: true } }
+    assert_not @other_user.reload.admin?
   end
 
-  test "should have delete links" do
-    first_page_of_users = User.where(activated: true).paginate(page: 1)
-    first_page_of_users.each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name
-      unless user == @admin
-        assert_select 'a[href=?]', user_path(user), text: 'delete'
-      end
-    end
-  end
-
-  test "should be able to delete non-admin user" do
-    assert_difference 'User.count', -1 do
-      delete user_path(@non_admin)
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
     end
     assert_response :see_other
-    assert_redirected_to users_url
+    assert_redirected_to login_url
   end
 
-  test "should display only activated users" do
-    # ページにいる最初のユーザーを無効化する。
-    User.paginate(page: 1).first.toggle!(:activated)
-    # /usersを再度取得して、無効化済みのユーザーが表示されていないことを確かめる
-    get users_path      
-    # 表示されているすべてのユーザーが有効化済みであることを確かめる
-    assigns(:users).each do |user|
-      assert user.activated?
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
     end
+    assert_response :see_other
+    assert_redirected_to root_url
   end
-end
 
-class UsersNonAdminIndexTest < UsersIndex
+  test "should redirect following when not logged in" do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
 
-  test "should not have delete links as non-admin" do
-    log_in_as(@non_admin)
-    get users_path
-    assert_select 'a', text: 'delete', count: 0
+  test "should redirect followers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
   end
 end
--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -18,5 +18,7 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     @user.microposts.paginate(page:1).each do |micropost|
       assert_match micropost.content, response.body
     end
+    assert_select '#following', text: @user.following.count.to_s
+    assert_select '#followers', text: @user.followers.count.to_s
   end
 end
